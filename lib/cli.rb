467149cddb542417b6d8dd6d021ec155b89896c6
neivyx <janeivyhilario@gmail.com>
2020-07-12 08:31:47 -0400

    +class - init

---
+++ lib/cli.rb
@@ -0,0 +1,18 @@
+class CLI
+puts "hello1"
+    attr_accessor :input
+
+    def initialize
+        @input = input
+        @apit = @API.new
+    end
+
+    def call
+
+    end
+
+    def start
+        
+    end
+
+end
