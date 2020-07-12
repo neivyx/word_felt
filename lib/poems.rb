564c5c86c6bb67abed0db04c36a56c602a573bb6
neivyx <janeivyhilario@gmail.com>
2020-07-12 08:44:50 -0400

    +def init, attr_acc-name,author, self, array

--- lib/poems.rb
+++ lib/poems.rb
@@ -1,3 +1,18 @@
 class Poem
-    
+
+    attr_accessor :name, :author
+
+    @@all = []
+
+    def initialize(name, author)
+        @name = name
+        @author = author
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
+
+    puts "hello3"
 end
