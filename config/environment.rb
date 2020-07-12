d7f7ced7c7782c183e255e7fe47d4cc5ae6d9797
neivyx <janeivyhilario@gmail.com>
2020-07-12 08:31:07 -0400

    +require - colorize, httparty

--- config/environment.rb
+++ config/environment.rb
@@ -1,6 +1,13 @@
-require "word_felt/version"
+require "pry"
+require "httparty"
+require "colorize"
 
-module WordFelt
+require_relative "../lib/api.rb"
+require_relative "../lib/cli.rb"
+require_relative "../lib/poems.rb"
+
+
+module WordFelt #ignore
   class Error < StandardError; end
   # Your code goes here...
 end
