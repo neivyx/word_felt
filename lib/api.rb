f3ced3e6ae55e213f8171be9c96472e864562ea9
neivyx <janeivyhilario@gmail.com>
2020-07-12 15:24:11 -0400

    restructed httparty method

--- lib/api.rb
+++ lib/api.rb
@@ -2,29 +2,68 @@ require 'pry'
 require 'httparty'
 
 class API
-    puts "hello2"
-
-    URL = "https://poetrydb.org/author"
+    BASE_URL = "https://poetrydb.org/random/1"
 
     def get_poems
-        response = HTTParty.get(URL + ",linecount/author,title,lines")
-             puts response[rand(154)]["title"][i]
-    end
 
-    def get_longer_poems
-        response = HTTParty.get(URL + ",linecount/;21/author,title,lines").to_a
-        (0..20).each { |i| puts response[rand(154)]['lines'][i] }
-    end
+        response = HTTParty.get(BASE_URL)
+        response.each do |text|
+            Poem.new(text["author"], text["title"], text["lines"])
 
-    def get_shorter_poems
-        response = HTTParty.get(URL + ",linecount/;11/author,title,lines").to_a
-        (0..10).each { |i| puts response[rand(154)]['lines'][i] }
-
+           
+        end
     end
 
 
-end
 
+    end
+
+    # response = HTTParty.get('https://poetrydb.org/random/1')
+    # response.select {|i| puts "quoteText: 
+    #     #{i['lines']}, quoteAuthor:#{i['author']}\n\n, quoteTitle: #{i['title']}\n\n"}
+
+    #     final_poem = response.select do |i| puts "quoteText: 
+    #         #{i['lines']}, quoteAuthor:#{i['author']}, quoteTitle: #{i['title']}"
+
+    #     end
+
+            
+
+    #     def get_a_quote
+    #         url = "https://quote-garden.herokuapp.com/api/v2/quotes/random"
+    #        quote = HTTParty.get(url)
+    #        Quotes.new(quote["quote"]["quoteText"], quote["quote"]["quoteAuthor"])
+
+
+    
+    #transfer into array?
+    #interate 
+
+
+    # def get_poems
+    #     response = HTTParty.get(URL + ",linecount/author,title,lines")
+    #          puts response[rand(154)]["title"][i]
+    # end
+
+    # def get_longer_poems
+    #     response = HTTParty.get(URL + ",linecount/;14/author,title,lines").to_a
+    #     (0..13).each { |i| puts response[rand(154)]['lines'][i] }
+    # end
+
+    # def get_shorter_poems
+    #     response = HTTParty.get(URL + ",linecount/;11/author,title,lines").to_a
+    #     (0..10).each { |i| puts response[rand(154)]['lines'][i] }
+
+    # end
+
+
+    
+
+
+# end
+
+# https://poetrydb.org/random/3/title
+# https://poetrydb.org/random/3/lines
 
 # author = HTTParty.get('https://poetrydb.org/author')
 
@@ -35,8 +74,9 @@ end
 
 # https://poetrydb.org/author,linecount/Poe;14/lines
 
-https://poetrydb.org/random,linecount;10/lines
-https://poetrydb.org/random
+# https://poetrydb.org/random,linecount;10/lines
+# https://poetrydb.org/random/lines
+# https://poetrydb.org/random/
 
 # https://poetrydb.org/author/Poe/all
 
@@ -61,3 +101,13 @@ https://poetrydb.org/random
 # end
 
 # /<input field>/<search term>[;<search term>][..][:<search type>][/<output field>][,<output field>][..][.<format>]
+
+
+# Nadav 
+# code 
+# -- response.select {|i| puts "quoteText: #{i[‘lines’]}\n\n, quoteAuthor:#{i[‘author’]}\n\n, quoteTitle: #{i[‘title’]}\n\n"}
+
+
+# response = HTTParty.get('https://poetrydb.org/random/1')
+# response.select {|i| puts "quoteText: 
+#     #{i['lines']}, quoteAuthor:#{i['author']}\n\n, quoteTitle: #{i['title']}\n\n"}
