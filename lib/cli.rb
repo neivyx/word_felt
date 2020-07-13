312405ad20b909006a2be552f2416c3915c98ebe
neivyx <janeivyhilario@gmail.com>
2020-07-12 22:58:29 -0400

    colorize

--- lib/cli.rb
+++ lib/cli.rb
@@ -1,3 +1,6 @@
+require 'colorize'
+
+
 class CLI
 
     attr_accessor :input
@@ -15,24 +18,30 @@ class CLI
     end
 
     def greeting
+        line
+        line
         line 
-        puts "|| WordFelt: Anthology || "
+        puts "|| WordFelt: Anthology ||".blue
         line
-        puts "  >> Poetry Classics <<"
+        puts "  >> Poetry Classics <<".blue
         line
-        puts "     -by. neivy"
+        puts "     -by. neivy".blue
+        line
+        line
     end
 
     def ask
+        sleep(1)
         line
         puts "Enter 'yes' or 'no' to continue."
         line
     end
 
     def start
+        slee(1!)
         line
         puts "Would you like to retrieve a random poem, from the archives?"
-        print ""
+       
         line
         ask
         user_input = gets.chomp.to_s.downcase
@@ -55,8 +64,8 @@ class CLI
 
     def farewell
         line
-        puts "farewell my friend."
-        puts "\tfin."
+        puts "farewell my friend.".red
+        puts "\tfin.".red
         line
     end
 
@@ -65,16 +74,17 @@ class CLI
     end
 
     def reprint
+        sleep(5)
         puts "Would you like generate a new poem?"
         ask
         print ""
-        user_input = gets.strip.to_s.downcase
-        if user_input == "yes"
+        user_input = gets.strip.to_s.downcase.green
+        if user_input == "yes".green
             line
-            print_poem
+            print_poem.light_blue
             line
             reprint
-        else user_input == "no"
+        else user_input == "no".red
             line
             farewell
             line
@@ -85,7 +95,7 @@ class CLI
     end
 
     def validation
-        puts "Whoops. Give it another try!"
+        puts "Whoops. Give it another try!".red
     end
 
     def line
