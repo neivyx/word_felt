9056f9b6d93ef7d06b03189b5247628c688787da
neivyx <janeivyhilario@gmail.com>
2020-07-20 03:02:09 -0400

    +new menu, new print method, author list

--- lib/cli.rb
+++ lib/cli.rb
@@ -13,7 +13,8 @@ class CLI
 
     def call #start/call/run/launch
         greeting
-        get_input
+        menu
+        # get_input
         # start
 
     end
@@ -31,38 +32,70 @@ class CLI
         line
     end
 
-    def ask
-        line
-        puts "Enter 'yes' or 'no' to continue." #initial ask prompt
-        line
-    end
 
-    def start
-        sleep(1) #delay
-        line
-        puts "Would you like to retrieve a random poem, from the archives?"
-        line
-        ask
-        user_input = gets.chomp.to_s.downcase
-        if user_input == 'yes' 
-            line
-            bar
-            line # dividers / breaks
-            print_poem # prints poem
-            line # dividers / breaks
-            bar
-            reprint
-        elsif user_input == "no"
-            line
-          farewell
-            line
-        else
-            line
-            validation
-            line
+
+    def menu
+        # input = TTY::Prompt.new
+        # user_input = input.select("Print a random poem or Would like to see a list of authors?")
+        
+        prompt = TTY::Prompt.new
+        
+        choices = [
+            {name: 'Print a random poem', value: 1},
+            {name: 'Would like to see a list of authors?', value: 2},
+            {name: 'Exit', value: 3}
+                ]
+                user_input = prompt.select("Choose an option below", choices)
+        
+             case user_input
+        
+             when 1
+                line
+                print_poem
+                line
+                menu
+        
+             when 2 
+                line
+                list_authors
+                line
+                menu
+             when 3
+                line
+                farewell
+                line
+             end
+    end
+        
+   
+    def list_authors
+        
+        input = TTY::Prompt.new
+            user_input = input.select("Choose a author below to print a random poem", %w(William-Shakespeare Edgar-Allan-Poe Oscar-Wilde Emily-Dickinson William-Blake Exit))
+        case user_input
+            when "William-Shakespeare"
+            # random = rand(13..30)
+            @api.get_author_poems("shakespeare","14")
+            print_author_poem
+            when "Edgar-Allan-Poe"
+            @api.get_author_poems("poe","16")
+            print_author_poem
+            when "Oscar-Wilde"
+            @api.get_author_poems("wilde","25")
+            print_author_poem
+            when "Emily-Dickinson"
+            @api.get_author_poems("dickinson","15")
+            print_author_poem
+            when  "William-Blake"
+            @api.get_author_poems("blake","16")
+            print_author_poem
+            when "Exit"
+            menu
+        
         end
 
     end
+
 
     def farewell #end / no response
         line
@@ -73,17 +106,45 @@ class CLI
 
     def print_poem #api call
         API.new.get_poems
-        puts Poem.all.sample.author
-        # API.new.get_poems
+        # puts Poem.all.sample.title
+        # puts Poem.all.sample.author
+        puts Poem.all.sample.lines
+    
     end
 
-    def get_input
-        puts "type the name of a author"
-    author_input = gets.chomp
-    API.get_poems(author_input)
-    end
+    # def get_input
+    #     puts "type the name of a author"
+    # author_input = gets.chomp
+    # @api.get_poems("poe")
+    # Poem.all.sample.author
+    # binding.pry
+    # end
 
-    def reprint #loop back
+    def print_author_poem
+            # Author.all.each do |poem_hash|
+            # puts "#{poem_hash.title} #{poem_hash.author} #{poem_hash.lines}"
+             poem_data = []
+            #  print poem_data.first
+            
+            Author.all.each do |poem_hash|
+                poem_data << "#{poem_hash.title} #{poem_hash.author} #{poem_hash.lines}"
+            end
+            binding.pry
+            puts poem_data[get_input]
+
+            # binding.pry
+
+            #get_input = ask.("how many poem would you like to see", 1..10
+
+            
+            
+    # end
+
+    #poem_data = Author.all
+    #poem_data.first
+end
+
+    def reprint
         sleep(2)
         line
         puts "Would you like generate a new poem?"
@@ -106,7 +167,7 @@ class CLI
         
     end
 
-    def validation #error
+    def validation
         puts "Whoops. Give it another try!".red
     end
 
@@ -121,3 +182,5 @@ class CLI
      end
 
 end
+
+
